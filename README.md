ef3b16c25a188f90b9ef94d32b8a2ea149bd3943
Fabio-RibeiroB <fabio.brady@gmail.com>
2022-08-08 17:41:51 +0100

    Moving repo

---
+++ README.md
@@ -0,0 +1,7 @@
+# Astronomical Object Classification from The Sloan Digital Sky Survey Observations
+
+CREATE venv for the predict script.
+The deployment script has its own requirements file and env called rfpredict.
+Model too large to push to GitHub so can dowload here:
+https://drive.google.com/drive/folders/1a0T1jDLMTgfBx1qavTaVnuLmL8G4ydKr?usp=sharing
+Done within jupyter-lab virtual environment called myproj.
---
+++ model/StellarPredictRFC.py
@@ -0,0 +1,12 @@
+import joblib
+import pandas as pd
+import warnings
+warnings.filterwarnings("ignore")
+loaded_rf = joblib.load("./random_forest.joblib")
+filename=input('Enter file path:')
+data = pd.read_csv(filename)
+data = data[['u','g','r','i','z', 'redshift']]
+prediction = pd.DataFrame(loaded_rf.predict(data))
+prediction.to_csv('rf_predictions.csv')
+print(prediction.value_counts())
+prediction.to_csv('rf_predictions.csv')
