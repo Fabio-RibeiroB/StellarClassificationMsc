efb60141bde562e3fc874ed18de492c1b4c737b9
Fabio-RibeiroB <fabio.brady@gmail.com>
2022-08-25 11:54:39 +0100

    Final changes?

--- model/.ipynb_checkpoints/StellarPredictRFC-checkpoint.py
+++ model/.ipynb_checkpoints/StellarPredictRFC-checkpoint.py
@@ -1,11 +1,12 @@
-import pickle
+import joblib
 import pandas as pd
 import warnings
 warnings.filterwarnings("ignore")
-loaded_model = pickle.load(open('finalised_model.sav', 'rb'))
+loaded_rf = joblib.load("./random_forest.pkl")
 filename=input('Enter file path:')
 data = pd.read_csv(filename)
 data = data[['u','g','r','i','z', 'redshift']]
-prediction = pd.DataFrame(loaded_model.predict(data))
+prediction = pd.DataFrame(loaded_rf.predict(data))
 prediction.to_csv('rf_predictions.csv')
 print(prediction.value_counts())
+prediction.to_csv('rf_predictions.csv')
